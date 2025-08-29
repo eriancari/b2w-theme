0502b1deba061ad46bccd2e673303377fa7bca69
Erika Carriedo <ann@aplusanswers.com>
2025-08-29 18:37:19 +0800

    Cleanup CSS and Modify CSS for Wordpress

--- header.php
+++ header.php
@@ -55,7 +55,7 @@
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div class="container-fluid">
                     
-					<a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="Bootstrap to Wordpress"></a>
+					<a class="navbar-brand" href="#"><img src="<?= bloginfo('stylesheet_directory')?>/assets/img/logo.png" alt="Bootstrap to Wordpress"></a>
                     
 					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
