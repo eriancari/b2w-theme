1926dec5909716778926942664a7f4b400e44a24
Erika Carriedo <ann@aplusanswers.com>
2025-08-29 16:41:37 +0800

    make header menu from static template

--- header.php
+++ header.php
@@ -31,8 +31,10 @@
         <div class="navbar-wrapper">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="Bootstrap to Wordpress"></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    
+					<a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="Bootstrap to Wordpress"></a>
+                    
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
