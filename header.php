57981b4ef698ed7b83ac1e8e9bf1db9cb377a580
Erika Carriedo <ann@aplusanswers.com>
2025-08-29 17:23:59 +0800

    adding assets, scripts, fonts and images

--- header.php
+++ header.php
@@ -17,7 +17,30 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="<?= bloginfo('stylesheet_directory')?>/assets/css/bootstrap.min.css">
+
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
+    <!-- <link href='assets/css/font-awesome/css/fontawesome.min.css' rel='stylesheet' type='text/css'> -->
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+    <!-- Typekit font: Different font was used instead of proxima nova -- Source Sans Pro -->
+    <link rel="stylesheet" href="https://use.typekit.net/mgc5qmz.css">
+
 	<?php wp_head(); ?>
+
+	<!-- html5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
