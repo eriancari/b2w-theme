20b795f3fd3d9254b01930b52ee1beb79b933342
Erika Carriedo <ann@aplusanswers.com>
2025-09-03 15:43:44 +0800

    Code the 'Boost Your Income' Section with Advance Custom Fields

--- page-home.php
+++ page-home.php
@@ -13,6 +13,14 @@ $button_text = get_post_meta( 7, 'button_text', true); // post id in URL
 $optin_text = get_post_meta( 7, 'optin_text', true); // post id in URL 
 $optin_button_text = get_post_meta( 7, 'optin_button_text', true); // post id in URL 
 
+// Advanced Custom Fields
+$income_feature_img = get_field('income_feature_image');
+$income_section_title = get_field('income_section_title');
+$income_section_desc = get_field('income_section_description');
+$reason_1_title = get_field('reason_1_title');
+$reason_1_desc = get_field('reason_1_description');
+$reason_2_title = get_field('reason_2_title');
+$reason_2_desc = get_field('reason_2_description');
 get_header();
 ?>
 
@@ -73,25 +81,24 @@ get_header();
 <section id="boost-income">
     <div class="container">
         <div class="section-header">
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-boost.png" alt="Chart">
-            <h2>How You Can Boost Your Income</h2>
+            <!-- If user uploaded and image -->
+            <?php if ( !empty($income_feature_img) ) : ?>
+            <img src="<?= $income_feature_img['url'] ?>" alt="<?= $income_feature_img['alt'] ?>">
+            <?php endif; ?>
+            
+            <h2><?= $income_section_title; ?></h2>
         </div>
 
-        <p class="lead">Whether you’re a freelance designer, entrepreneur, employee for a company, code hobbyist, or
-            looking for a new career — this course gives you an immensely valuable skill that will enable you to
-            either:</p>
+        <p class="lead"><?= $income_section_desc; ?></p>
 
         <div class="row">
             <div class="col-sm-6">
-                <h3>Make money on the side</h3>
-                <p>So you can save up for that Hawaiian vacation you’ve been wanting, help pay off your debt, your
-                    car, your mortgage, or simply just to have bonus cash laying around.</p>
+                <h3><?= $reason_1_title; ?></h3>
+                <p><?= $reason_1_desc; ?></p>
             </div>
             <div class="col-sm-6">
-                <h3>Create a full-time income</h3>
-                <p>WordPress developers have options. Many developers make a generous living off of creating custom
-                    WordPress themes and selling them on websites like ThemeForest. Freelance designers and
-                    developers can also take on WordPress projects and make an extra $1,000 - $5,000+ per month.</p>
+                <h3><?= $reason_2_title; ?></h3>
+                <p><?= $reason_2_desc; ?></p>
             </div>
         </div>
     </div>
