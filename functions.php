e0f3014f23844ba88ae90629a369789542004583
Erika Carriedo <ann@aplusanswers.com>
2025-10-09 19:05:53 +0800

    Code the individual blog excerpts

--- functions.php
+++ functions.php
@@ -176,3 +176,13 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/** 
+ * Replaces the excerpt "more" text by a link
+ */
+
+function new_excerpt_more($more) {
+	global $post;
+
+	return '... <a class="moretag" href="' . get_permalink($post->ID) . '"> continue reading </a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
--- template-parts/content.php
+++ template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -12,52 +13,35 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+		if (is_singular()) :
+			the_title('<h1 class="entry-title">', '</h1>');
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h3>');
 		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				bootstrap2wordpress_posted_on();
-				bootstrap2wordpress_posted_by();
-				?>
-			</div><!-- .entry-meta -->
+		if ('post' === get_post_type()) :
+		?>
+			<div class="post-details">
+				<i class="fa fa-user"></i> <?php the_author(); ?>
+				<i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+				<i class="fa fa-folder"></i> <?php the_category(", ",) ?>
+				<i class="fa fa-tags"></i> <?php the_tags('', ', ', '') ?>
+
+				<div class="post-comments-badge">
+					<a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%'); ?></a>
+				</div><!-- post-comments-badge -->
+				<?php edit_post_link('Edit', '<i class="fa fa-pencil"></i>', ''); ?>
+			</div><!-- post-details -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php bootstrap2wordpress_post_thumbnail(); ?>
+	<?php if (has_post_thumbnail()) { // check for feature image ?>
+		<div class="post-image">
+			<?php the_post_thumbnail(); ?>
+		</div><!-- post-image -->
+	<?php } ?>
+	
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bootstrap2wordpress' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bootstrap2wordpress' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php bootstrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+	<div class="post-excerpt">
+		<?php the_excerpt(); ?>
+	</div><!-- post-excerpt -->
