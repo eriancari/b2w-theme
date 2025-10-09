9ff8e2deb3106560a42ec14c873dadb2509d49b7
Erika Carriedo <ann@aplusanswers.com>
2025-10-09 21:13:27 +0800

    Code the full blog post

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
@@ -9,32 +10,41 @@
 
 get_header();
 ?>
+<!-- MAIN CONTENT
+	================================================== -->
+<div class="container">
+	<div class="row" id="primary">
 
-	<main id="primary" class="site-main">
+		<main id="content" class="col-sm-8">
+			<?php
+			while (have_posts()) :
+				the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				get_template_part('template-parts/content', 'single');
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'bootstrap2wordpress') . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'bootstrap2wordpress') . '</span> <span class="nav-title">%title</span>',
+					)
+				);
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'bootstrap2wordpress' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'bootstrap2wordpress' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+				// If comments are open or we have at least one comment, load up the comment template.
+				if (comments_open() || get_comments_number()) :
+					comments_template();
+				endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			endwhile; // End of the loop.
+			?>
 
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+		</main><!-- #main -->
+		<!-- SIDEBAR
+			================================================== -->
+		<aside class="col-sm-4">
+			<?php get_sidebar(); ?>
+		</aside>
+	</div>
+</div>
 
 <?php
-get_sidebar();
 get_footer();
--- template-parts/content.php
+++ template-parts/content.php
@@ -11,7 +11,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header>
 		<?php
 		if (is_singular()) :
 			the_title('<h1 class="entry-title">', '</h1>');
@@ -40,8 +40,8 @@
 			<?php the_post_thumbnail(); ?>
 		</div><!-- post-image -->
 	<?php } ?>
-	
 
 	<div class="post-excerpt">
 		<?php the_excerpt(); ?>
 	</div><!-- post-excerpt -->
+</article>
