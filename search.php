6740fb096c39b20275f1bc1c7d6e06488883cc48
Erika Carriedo <ann@aplusanswers.com>
2025-10-13 17:03:30 +0800

    Customize the search result template

--- search.php
+++ search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying search results pages
  *
@@ -9,45 +10,51 @@
 
 get_header();
 ?>
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+	<h1 class="page-title">
+		<?php
+		/* translators: %s: search query. */
+		printf(esc_html__('Search Results for: %s', 'bootstrap2wordpress'), '<span>' . get_search_query() . '</span>');
+		?>
+	</h1>
+</section>
 
-	<main id="primary" class="site-main">
+<div class="container">
+	<div id="primary" class="row">
+		<main id="content" class="col-sm-8">
 
-		<?php if ( have_posts() ) : ?>
+			<?php if (have_posts()) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'bootstrap2wordpress' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
+
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+				/* Start the Loop */
+				while (have_posts()) :
+					the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part('template-parts/content', 'search');
 
-			endwhile;
+				endwhile;
 
-			the_posts_navigation();
+				the_posts_navigation();
 
-		else :
+			else :
 
-			get_template_part( 'template-parts/content', 'none' );
+				get_template_part('template-parts/content', 'none');
 
-		endif;
-		?>
-
-	</main><!-- #main -->
+			endif;
+			?>
+		</main> <!-- #content -->
+		<aside class="col-sm-4">
+			<?php get_sidebar(); ?>
+		</aside>
+	</div> <!-- #primary -->
+</div> <!-- .container -->
 
 <?php
-get_sidebar();
 get_footer();
