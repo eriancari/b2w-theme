a1fd5a3820f862880efcf3f13da618be691897ed
Erika Carriedo <ann@aplusanswers.com>
2025-10-13 16:55:10 +0800

    Customize the archive template

--- archive.php
+++ archive.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
@@ -10,20 +11,21 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<?php if (have_posts()) : ?>
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
 
-		<?php if ( have_posts() ) : ?>
+		<?php
+		the_archive_title('<h1 class="page-title">', '</h1>');
+		the_archive_description('<small class="archive-description">', '</small>');
+		?>
+	</section>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
+	<div class="container">
+		<div id="primary" class="row">
+			<main id="content" class="col-sm-8">
 			<?php
 			/* Start the Loop */
-			while ( have_posts() ) :
+			while (have_posts()) :
 				the_post();
 
 				/*
@@ -31,7 +33,7 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part('template-parts/content', get_post_type());
 
 			endwhile;
 
@@ -39,13 +41,16 @@ get_header();
 
 		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+			get_template_part('template-parts/content', 'none');
 
 		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+			?>
+			</main>
+			<!-- SIDEBAR -->
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside>
+		</div>
+	</div>
+	<?php
+	get_footer();
